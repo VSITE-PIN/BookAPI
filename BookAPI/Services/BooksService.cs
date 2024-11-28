3a92c0e9f804fbbc9f0b35ac861f3f1c07003af5
Davor Petrlić <dapetrli@vsite.hr>
2024-11-28 18:04:59 +0100

    prvi dio i malo drugog

---
+++ BookAPI/Services/BooksService.cs
@@ -0,0 +1,31 @@
+﻿using BookAPI.Data;
+using static BookAPI.Controllers.BooksController;
+
+namespace BookAPI.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddBook(BookVM book)
+        {
+            var newBook = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead : null,
+                Rate = book.IsRead ? book.Rate : null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverPictureURL = book.CoverPictureURL,
+                DateAdded = DateTime.Now,
+            };
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+        }
+    }
+}
