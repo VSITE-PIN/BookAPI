8e1391b2ae43b8b47c540d3e441f49e5bc54bc0b
breski-igor <igor.breski@vsite.hr>
2024-11-28 18:04:49 +0100

    igor_breski

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,6 +1,16 @@
+using BookAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+    ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
