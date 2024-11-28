85e7a1c37df3357e08dc674cdf6756bac75416b2
Iva Brletić <ivbrleti@vsite.hr>
2024-11-28 17:48:40 +0100

    prvi dio

---
+++ BookAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookAPI.NewFolder;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) :
+	   base(options)
+		{ }
+		public DbSet<Book> Books { get; set; }
+	}
+
+}
