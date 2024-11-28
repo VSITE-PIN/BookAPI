57f1f6da4a6faa1da560983251763b0c25f488a1
Mia Fidermuc Vekić <mifiderm@vsite.hr>
2024-11-28 18:42:57 +0100

    predavanje 28.11.

---
+++ BookAPI/Data/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using BookAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Data
+{
+    public class BookVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; } //Da li smo pročitali knjigu
+        public bool IsRead { get; set; } //datum kada smo ju pročitali (nullable Datetime - ako ju nismo pročitala datum će imati vrijednost null)
+        public DateTime? DateRead { get; set; } //ocjena (nullable int - ako ju nismo pročitali ne možemo ju ocijeniti pa će vrijednost biti null)
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string CoverPictureURL { get; set; }
+    }
+    public class BooksController : ControllerBase
+    {
+        public BooksService BooksService { get; set; }
+        public BooksController(BooksService booksService)
+        {
+            BooksService = booksService;
+        }
+        [HttpPost]
+        public IActionResult AddBook([FromBody] BookVM book)
+        {
+            BooksService.AddBook(book);
+            return Ok();
+        }
+    }
+
+
+}
--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,6 +1,16 @@
+using BookAPI.Data;
+using BookAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContex>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+ ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
+builder.Services.AddScoped<BooksService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
