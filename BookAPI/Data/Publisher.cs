3ed4ff6cd010771f508ca449a7dc9947db5d9040
letomic3 <letomic@vsite.hr>
2024-11-28 18:55:41 +0100

    3.dio

---
+++ BookAPI/Data/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAPI.Data
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+}
