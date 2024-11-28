09b54fa3f39a4da8bcbad395686d834bc610cdca
veronikamataija1 <vemataija@vsite.hr>
2024-11-28 18:55:37 +0100

    28.11.predavanje-3.dio

--- BookAPI/Data/Book.cs
+++ BookAPI/Data/Book.cs
@@ -17,5 +17,12 @@
         public string Author { get; set; }
         public string CoverPictureURL { get; set; }
         public DateTime DateAdded { get; set; }
+
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+
+        public List<BookAuthor> BookAuthors { get; set; }
+
+
     }
 }
