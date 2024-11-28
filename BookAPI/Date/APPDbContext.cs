5296839c950665e582532cd32f8d4b1eeb3a3c1e
Marko Vinković <mavinkov@vsite.hr>
2024-11-28 17:44:30 +0100

    Predavanje

---
+++ BookAPI/Date/APPDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookAPI.Date
+{
+    public class APPDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+    base(options){ }
+public DbSet<Book> Books{get; set;}
+}
