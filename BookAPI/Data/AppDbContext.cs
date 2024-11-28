971240d82c81a4578ffa846a3e7a2960e94d3e5e
Lara Mihelin <lamihelin@vsite.hr>
2024-11-28 18:56:57 +0100

    Third path done

--- BookAPI/Data/AppDbContext.cs
+++ BookAPI/Data/AppDbContext.cs
@@ -8,5 +8,21 @@ namespace BookAPI.Data
             base(options)
             { }
             public DbSet<Book> Books { get; set; }
+            public DbSet<Publisher> Publishers { get; set; }
+            public DbSet<Author> Authors { get; set; }
+            public DbSet<BookAuthor> BooksAuthors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookAuthor>()
+            .HasOne(b => b.Book)
+            .WithMany(ba => ba.BookAuthors)
+            .HasForeignKey(bi => bi.BookId);
+            modelBuilder.Entity<BookAuthor>()
+            .HasOne(b => b.Author)
+            .WithMany(ba => ba.BookAuthors)
+            .HasForeignKey(bi => bi.AuthorId);
         }
+    }
 }
