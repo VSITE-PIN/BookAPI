2f4b253fb7287219b3f9532ec7785e7f457f22a6
ivan0301 <ivangrgic0301@gmail.com>
2024-11-28 18:36:41 +0100

    napravljeno do relacijskih

---
+++ BookAPI/ViewModel/BookVM.cs
@@ -0,0 +1,18 @@
+﻿namespace BookAPI.ViewModel
+{
+    public class BookVM
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //Da li smo pročitali knjigu
+        public bool IsRead { get; set; }
+        //datum kada smo ju pročitali (nullable Datetime - ako ju nismo pročitala datum će imati vrijednost null)
+ public DateTime? DateRead { get; set; }
+        //ocjena (nullable int - ako ju nismo pročitali ne možemo ju ocijenitipa će vrijednost biti null)
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string CoverPictureURL { get; set; }
+    }
+}
+
