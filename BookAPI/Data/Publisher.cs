4ca47c5cf59b8dab2639ebe3e62bf38b44e3aa9c
krvukova <krvukova@vsite.hr>
2024-11-28 18:47:30 +0100

    commit 4

---
+++ BookAPI/Data/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Data
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Book> Books { get; set; }
+
+        public DbSet<Publisher> Publishers { get; set; }
+
+    }
+}
---
+++ BookAPI/Migrations/20241128174040_AddPublisher.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublisher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PublisherId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublisherId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Publisher_Publisher_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "Publisher",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books",
+                column: "PublisherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Publisher_PublisherId",
+                table: "Publisher",
+                column: "PublisherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publisher_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publisher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publisher_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublisherId",
+                table: "Books");
+        }
+    }
+}
