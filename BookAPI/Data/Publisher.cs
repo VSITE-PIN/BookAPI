0e8b06baf38b044e4ce6e1bd032a2c1e81b858f5
Bruno Barbarić <brbarbar@vsite.hr>
2024-11-29 19:08:13 +0100

    predavanje 28.11

---
+++ BookAPI/Data/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Data;
+
+namespace BookAPI.Data
+{
+    public class Publisher
+    {
+        public List<Book> Books { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<BooksService>();
+
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+ ?? throw new InvalidOperationException("Connection stringDefaultConnString not found.")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
