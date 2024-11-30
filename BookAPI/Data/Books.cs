aca8622566b8e9e82568fb8e3db1295ad09c725c
kakrulja <karlo.kruljac@vsite.hr>
2024-11-30 23:58:53 +0100

    Predavanje

---
+++ BookAPI/Data/Books.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Data
+{
+    public class Books : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            
+            public bool IsRead { get; set; }
+            
+            public DateTime? DateRead { get; set; }
+            
+            public int? Rate { get; set; }
+            public string Genre { get; set; }
+            public string Author { get; set; }
+            public string CoverPictureURL { get; set; }
+            public DateTime DateAdded { get; set; }
+        }
+
+    }
+}
