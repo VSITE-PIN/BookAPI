57f7fd3d77edf8b98d3b62bd8df051b18fdce483
vmkvsite <vikronja@vsite.hr>
2024-12-05 18:35:19 +0100

    Blabla

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+
+using Microsoft.AspNetCore.Mvc;
+using BookAPI.Data;
+using BookAPI.Services;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksService _booksService;
+
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _booksService.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody] BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            if (updatedBook == null)
+                return NotFound();
+
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _booksService.DeleteBook(id);
+            return Ok();
+        }
+    }
+}
--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,11 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+using BookAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")));
 
 var app = builder.Build();
 
